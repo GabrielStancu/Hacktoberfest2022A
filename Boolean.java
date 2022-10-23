0fa9b88c76830f0d3427da7c251f1c311267393c
sahiltagunde <72329866+sahiltagunde@users.noreply.github.com>
2022-10-23 22:15:21 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,16 @@
+// java code to demonstrate
+// Boolean toString() method
+
+class Boolean {
+    public static void main(String[] args) {
+
+        // boolean type value
+        boolean value = true;
+
+        // static toString() method of Boolean class
+        String output = Boolean.toString(value);
+
+        // printing the value
+        System.out.println(output);
+    }
+}
