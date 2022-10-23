06f9f379f5ac0538756e1bfea2f880a874750bd4
Daniel Bancos <57346365+DaniBan@users.noreply.github.com>
2022-10-23 17:16:08 +0300

    added main

--- InsertionSort.java
+++ InsertionSort.java
@@ -14,4 +14,16 @@ public class InsertionSort {
             arr[j + 1] = key;
         }
     }
+ 
+    public static void main(String args[])
+    {
+        int arr[] = {2, 11, 7, 5, 1, 3};
+ 
+        InsertionSort sorter = new InsertionSort();
+        ob.sort(arr);
+		
+		int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+    }
 }
