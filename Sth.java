b25cff71a174cbfaaea71455813b95ab671920f4
Buciuman Adrian <adieuadi@gmail.com>
2022-10-23 14:56:18 +0300

    add int sum

--- Sth.java
+++ Sth.java
@@ -1,5 +1,10 @@
 class Main {
+
+    int sum(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Using int sum " + sum(2, 3));
     }
 }
