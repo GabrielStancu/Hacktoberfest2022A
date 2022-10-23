3d487aebe743adfec8c8d0e882633d0f680e8193
sahiltagunde <72329866+sahiltagunde@users.noreply.github.com>
2022-10-23 22:16:10 +0530

    Add files via upload

---
+++ GFG.java
@@ -0,0 +1,16 @@
+// java code to demonstrate
+// Boolean toString() method
+
+class GFG {
+    public static void main(String[] args) {
+
+        // creating a Boolean object
+        Boolean b = new Boolean(true);
+
+        // toString method of Boolean class
+        String output = b.toString();
+
+        // printing the output
+        System.out.println(output);
+    }
+}
